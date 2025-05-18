ecc493ccacbb884de11bfc66e31e3e049c38aa7e
valteem <val.teem.scn@gmail.com>
2025-05-18 20:25:54 +0300

    advanced initializion: comma initializer (block matrix)

--- tests/01-dense-matrix/06-advanced-initialization.cc
+++ tests/01-dense-matrix/06-advanced-initialization.cc
@@ -14,3 +14,16 @@ TEST(AdvancedInitialization, CommaInitializerMatrix) {
     EXPECT_PRED2(MatricesApproxXi, output, expected);
 
 }
+
+TEST(AdvancedInitialization, CommaInitializerBlockMatrix) {
+
+    Eigen::MatrixXi input {{2, 4}, {6, 8}};
+
+    Eigen::MatrixXi output(4, 4);
+    output << input, input *2, input/2, input;
+
+    Eigen::MatrixXi expected {{2, 4, 4, 8}, {6, 8, 12, 16}, {1, 2, 2, 4}, {3, 4, 6, 8}};
+
+    EXPECT_PRED2(MatricesApproxXi, output, expected);
+
+}
